221e78e623842f080be8ee218a87b601e7d012db
Daniel Bravo <daniel.brv@hotmail.cl>
2021-10-06 16:03:55 -0300

    Se agrega GET: recivir parámetros

--- index.js
+++ index.js
@@ -6,6 +6,40 @@ app.get('/',(req,res) => {
     res.send('Hola mi server en express')
 })
 
+app.get('/nueva-ruta',(req,res) => {
+  res.send('Hola soy una nueva ruta')
+})
+
+app.get('/products',(req,res) => {
+  res.json([
+    {
+      name: 'Producto1',
+      Precio: 1000
+    },
+    {
+      name: 'Producto2',
+      Precio: 2000
+    }
+  ])
+})
+
+app.get('/products/:id', (req,res)=> {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'Producto2',
+    Precio: 2000
+  });
+})
+
+app.get('/categories/:categoryId/products/:productsId', (req, res) => {
+  const { categoryId, productsId } = req.params;
+  res.json({
+    categoryId,
+    productsId,
+  })
+})
+
 app.listen(port, () => {
   console.log('Mi port' + port);
 })
