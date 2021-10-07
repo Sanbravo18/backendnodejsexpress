058cdb51b78cdca43ebbc64cc66b168788da7115
Daniel Bravo <daniel.brv@hotmail.cl>
2021-10-07 13:19:42 -0300

    Se agrega Async Await

--- services/products.service.js
+++ services/products.service.js
@@ -20,46 +20,48 @@ class PoductsService {
   }
 
   // Funciones para los servicios
-  create(data) {
+  async create(data) {
     const newProduct = {
       id: faker.datatype.uuid(),
-      ...data
-    }
+      ...data,
+    };
     this.products.push(newProduct);
     return newProduct;
   }
 
   find() {
-    // Devolvemos los datos
-    return this.products;
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.products);
+      }, 5000);
+    });
   }
 
-  findOne(id) {
+  async findOne(id) {
     // Devolvemos un solo dato, para saber mas ir a manipulación de arrays
-    return this.products.find(item => item.id == id);
+    return this.products.find((item) => item.id == id);
   }
 
-  update(id,changes) {
-    const index = this.products.findIndex(item => item.id == id);
+  async update(id, changes) {
+    const index = this.products.findIndex((item) => item.id == id);
     if (index === -1) {
-      throw new Error('Produc not found')
+      throw new Error('Produc not found');
     }
     const product = this.products[index];
-    this.products[index]= {
+    this.products[index] = {
       ...product, //persiste los datos que no son modificados
-      ...changes
-
+      ...changes,
     };
-    return this.products [index]
+    return this.products[index];
   }
 
-  delete(id) {
-    const index = this.products.findIndex(item => item.id == id);
+  async delete(id) {
+    const index = this.products.findIndex((item) => item.id == id);
     if (index === -1) {
-      throw new Error('Produc not found')
+      throw new Error('Produc not found');
     }
-    this.products.splice(index,1);
-    return {id}
+    this.products.splice(index, 1);
+    return { id };
   }
 }
 
